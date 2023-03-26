4b13c5939330a7592936abddf2069cd4299ed71b
abhishek90894 <abhisheksrivastava90894@gmail.com>
2023-03-26 15:57:33 +0530

    first commit

---
+++ src/main/java/com/boot/Curdproject/curdProject/CurdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.boot.Curdproject.curdProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/Curdproject/curdProject/CurdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.Curdproject.curdProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
