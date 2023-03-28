63d49012847417a848fb25aa4212c7903e93fc71
abhishek90894 <abhisheksrivastava90894@gmail.com>
2023-03-29 00:38:03 +0530

    spring security authorisation configuration completed

--- src/main/java/com/boot/Curdproject/curdProject/config/securityConfig.java
+++ src/main/java/com/boot/Curdproject/curdProject/config/securityConfig.java
@@ -4,8 +4,11 @@ import com.boot.Curdproject.curdProject.entities.user;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -14,13 +17,20 @@ public class securityConfig
     @Bean
     public UserDetailsService userDetailsService()
     {
-//      UserDetails normal = user.builder().userName("abhishek")
-//                .Password("12345")
-//                .build();
-//
-//
-//
-//                return new InMemoryUserDetailsManager();
-//
+     UserDetails normal = User.builder().username("abhishek")
+             .password(passwordEncoder().encode("12345"))
+             .roles("NORMAL")
+             .build();
+     UserDetails admin = User.builder().username("ankur")
+             .password(passwordEncoder().encode("12345"))
+             .roles("ADMIN")
+             .build();
+              return new InMemoryUserDetailsManager(normal,admin);
+
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder()
+    {
+        return  new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/com/boot/Curdproject/curdProject/controllers/AuthController.java
@@ -0,0 +1,4 @@
+package com.boot.Curdproject.curdProject.controllers;
+
+public class AuthController {
+}
