147fb0586f6c6907996dd9bf1c77e086c7d3e4ad
abhishek90894 <abhisheksrivastava90894@gmail.com>
2023-03-29 17:14:44 +0530

    some file changes

--- src/main/java/com/boot/Curdproject/curdProject/repository/userRepository.java
+++ src/main/java/com/boot/Curdproject/curdProject/repository/userRepository.java
@@ -2,10 +2,13 @@ package com.boot.Curdproject.curdProject.repository;
 
 import com.boot.Curdproject.curdProject.entities.user;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+@Component
 public interface userRepository extends JpaRepository<user,String> {
-//    Optional<user> findByEmail(String email);
+//   Optional<user> findByEmail(String email);
 
 //    Optional<Object> findByUserName(String username);
 
