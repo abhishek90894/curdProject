9f438378c91c97cfe53a857df1c9a1d038e0195d
abhishek90894 <abhisheksrivastava90894@gmail.com>
2023-03-31 18:12:46 +0530

    implementing validation in user entity

--- src/main/java/com/boot/Curdproject/curdProject/entities/user.java
+++ src/main/java/com/boot/Curdproject/curdProject/entities/user.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,14 +20,21 @@ public class user  {
 
     @Id
     private String userId;
-
+    @NotNull
     private String userName;
     //@Column(unique = true)
+    @Email
     private String userEmail;
+    @Max(value = 12)
+    @Min(value = 6)
     private String Password;
+    @Min(4)
+    @Max(10)
     private String gender;
+    @NotBlank
     @Column(length =100)
     private String about;
+    @NotNull
     private String imageName;
 
 }
