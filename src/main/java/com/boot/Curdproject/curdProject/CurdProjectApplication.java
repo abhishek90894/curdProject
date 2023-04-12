e58f8b9e696f67e6a2b902f34e59ea9ed6ba5215
abhishek90894 <abhisheksrivastava90894@gmail.com>
2023-04-12 17:44:30 +0530

    fourth commit

--- src/main/java/com/boot/Curdproject/curdProject/CurdProjectApplication.java
+++ src/main/java/com/boot/Curdproject/curdProject/CurdProjectApplication.java
@@ -2,6 +2,10 @@ package com.boot.Curdproject.curdProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.net.SocketTimeoutException;
+import java.io.IOException;
 
 @SpringBootApplication
 public class CurdProjectApplication {
@@ -10,7 +14,18 @@ public class CurdProjectApplication {
 	{
 
 		SpringApplication.run(CurdProjectApplication.class, args);
-		System.out.println(System.getProperty("java.class.path"));
+		String hostname = "localhost";
+		int port = 9999;
+		int timeout = 5000; // 5 seconds
+
+		try (Socket socket = new Socket()) {
+			socket.connect(new InetSocketAddress(hostname, port), timeout);
+
+		} catch (SocketTimeoutException e) {
+			System.err.println("Socket connection timed out");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
